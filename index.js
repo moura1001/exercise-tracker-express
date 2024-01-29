fcc1e3d5cb8b96aab6af43c258630f5acf4232b1
moura1001 <gmouraneto01@gmail.com>
2024-01-29 21:07:06 +0000

    feat: add endpoints for managing registration and listing users

--- index.js
+++ index.js
@@ -2,12 +2,39 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const bodyParser = require("body-parser");
+
+const users = new Map();
+const exercises = new Map();
+
+const HEX_STRING_DEFAULT_SIZE = 24;
+const genRanHex = (size) =>
+  [...Array(size)]
+    .map(() => Math.floor(Math.random() * 16).toString(16))
+    .join("");
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+app
+  .route("/api/users")
+  .post(function (req, res) {
+    const username = req.body.username;
+    const _id = genRanHex(HEX_STRING_DEFAULT_SIZE);
+    users.set(_id, username);
+    res.json({ username, _id });
+  })
+  .get(function (req, res) {
+    const usersArray = Array.from(users.entries()).map(([id, username]) => ({
+      _id: id,
+      username,
+    }));
+    res.json(usersArray);
+  });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
